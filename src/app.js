6eb28564c77f393ea3ab9abca10d52e050f75148
rodriguc <crodriguez@groupemutuel.ch>
2018-01-09 16:57:16 +0100

    wip: lesson 78

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 // install -> import -> use
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "normalize.css/normalize.css";
 import "./styles/styles.scss";
 
@@ -15,13 +15,18 @@ const EditExpensePage = () => <div>This is from my Edit component</div>;
 
 const HelpPage = () => <div>This is from my Help component</div>;
 
+const NotFoundPage = () => <div>404!</div>;
+
 const routes = (
   <BrowserRouter>
     <div>
-      <Route path="/" component={ExpenseDashboardPage} exact={true} />
-      <Route path="/create" component={AddExpensePage} />
-      <Route path="/edit" component={EditExpensePage} />
-      <Route path="/help" component={HelpPage} />
+      <Switch>
+        <Route path="/" component={ExpenseDashboardPage} exact={true} />
+        <Route path="/create" component={AddExpensePage} />
+        <Route path="/edit" component={EditExpensePage} />
+        <Route path="/help" component={HelpPage} />
+        <Route component={NotFoundPage} />
+      </Switch>
     </div>
   </BrowserRouter>
 );
