4c5bd8e547a89b4ce1413142e0eae033d408b4fa
rodriguc <crodriguez@groupemutuel.ch>
2018-01-15 09:17:30 +0100

    end of lesson 79

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 // install -> import -> use
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch, Link, NavLink } from "react-router-dom";
 import "normalize.css/normalize.css";
 import "./styles/styles.scss";
 
@@ -15,11 +15,34 @@ const EditExpensePage = () => <div>This is from my Edit component</div>;
 
 const HelpPage = () => <div>This is from my Help component</div>;
 
-const NotFoundPage = () => <div>404!</div>;
+const NotFoundPage = () => (
+  <div>
+    404! - <Link to="/">Go home</Link>
+  </div>
+);
+
+const Header = () => (
+  <header>
+    <h1>Expensify</h1>
+    <NavLink to="/" activeClassName="is-active" exact={true}>
+      Dashboard
+    </NavLink>
+    <NavLink to="/create" activeClassName="is-active" exact={true}>
+      Create expense
+    </NavLink>
+    <NavLink to="/edit" activeClassName="is-active" exact={true}>
+      Edit Expense
+    </NavLink>
+    <NavLink to="/help" activeClassName="is-active" exact={true}>
+      Help
+    </NavLink>
+  </header>
+);
 
 const routes = (
   <BrowserRouter>
     <div>
+      <Header />
       <Switch>
         <Route path="/" component={ExpenseDashboardPage} exact={true} />
         <Route path="/create" component={AddExpensePage} />
