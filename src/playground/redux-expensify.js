0f48b9d03a4e089dbc6fc209918043651075e7fb
rodriguc <crodriguez@groupemutuel.ch>
2018-02-01 12:18:39 +0100

    end of lesson 93

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -1,7 +1,26 @@
 import { createStore, combineReducers } from "redux";
+import uuid from "uuid";
 
 //ADD_EXPENSE
+const addExpense = (
+  { description = "", note = "", amount = 0, createdAt = 0 } = {}
+) => ({
+  type: "ADD_EXPENSE",
+  expense: {
+    id: uuid(),
+    description,
+    note,
+    amount,
+    createdAt
+  }
+});
+
 //REMOVE_EXPENSE
+const removeExpense = ({ id } = {}) => ({
+  type: "REMOVE_EXPENSE",
+  id
+});
+
 //EDIT_EXPENSE
 //SET_TEXT_FILTER
 //SORT_BY_DATE
@@ -13,6 +32,15 @@ import { createStore, combineReducers } from "redux";
 const expensesReducerDefaultState = [];
 const expensesReducer = (state = expensesReducerDefaultState, action) => {
   switch (action.type) {
+    case "ADD_EXPENSE":
+      return [...state, action.expense]; //Avec un spread operator
+    // =  return state.concat(action.expense); //retourne un nouvel état.
+    //L'état étant un tableau, on utilise concat pour créer une copie de ce tableau en ajoutant l'élément en paramètre
+    //On ne doit jamais modifier directgement l'état
+
+    case "REMOVE_EXPENSE":
+      return state.filter(({ id }) => id !== action.id);
+
     default:
       return state;
   }
@@ -37,7 +65,21 @@ const store = createStore(
   combineReducers({ expenses: expensesReducer, filters: filtersReducer })
 );
 
-console.log(store.getState());
+store.subscribe(() => {
+  console.log(store.getState());
+});
+
+const expenseOne = store.dispatch(
+  addExpense({ description: "Rent", amount: 100 })
+);
+const expenseTwo = store.dispatch(
+  addExpense({ description: "Coffee", amount: 300 })
+);
+
+store.dispatch(removeExpense({ id: expenseOne.expense.id }));
+
+console.log(expenseOne);
+console.log(expenseTwo);
 
 //dummy data
 const demoState = {
