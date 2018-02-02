dc4f763bc834ae45e48753334082b1af2d4c89e5
rodriguc <crodriguez@groupemutuel.ch>
2018-02-02 10:59:43 +0100

    end of lesson 101

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 // install -> import -> use
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 import AppRouter from "./routers/AppRouter";
 import configureStore from "./store/configureStore";
 import { addExpense } from "./actions/expenses";
@@ -16,8 +17,18 @@ store.dispatch(addExpense({ description: "Water Bill" }));
 store.dispatch(addExpense({ description: "Gas Bill" }));
 store.dispatch(setTextFilter("water"));
 
+setTimeout(() => {
+  store.dispatch(setTextFilter("rent"));
+}, 3000);
+
 const state = store.getState();
 
 console.log(getVisibleExpenses(state.expenses, state.filters));
 
-ReactDOM.render(<AppRouter />, document.getElementById("app"));
+const jsx = (
+  <Provider store={store}>
+    <AppRouter />
+  </Provider>
+);
+
+ReactDOM.render(jsx, document.getElementById("app"));
