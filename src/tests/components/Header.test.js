37bb008fd382008ad1dc7f654fd34f168d67a8a4
rodriguc <crodriguez@groupemutuel.ch>
2018-03-01 15:54:30 +0100

    End of lesson 119

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,11 +1,11 @@
 import React from "react";
-import ReactShallowRenderer from "react-test-renderer/shallow";
+import {shallow} from "enzyme";
+
 import Header from "../../components/Header";
 
 test("should render Header correctly", () => {
-  const renderer = new ReactShallowRenderer();
-  renderer.render(<Header />);
-  //la 1re fois le snapshot sera créé (dans tests/_snapshots_). La deuxième fois il sera comparé
-  expect(renderer.getRenderOutput()).toMatchSnapshot();
-  console.log(renderer.getRenderOutput());
+  const wrapper = shallow(<Header/>);
+  expect(wrapper).toMatchSnapshot();
+
+//  expect(wrapper.find("h1").text()).toBe('Expensify');
 });
